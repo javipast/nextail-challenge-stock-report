62a6fae50235020ee821889d940bb72696e54cd6
javipast <javier.pastor.lozano@gmail.com>
2022-09-09 13:59:36 +0100

    app.js class created with json data fetch

--- js/app.js
+++ js/app.js
@@ -1,2 +1,26 @@
 import StockoutInfo from './components/stockout-info/StockoutInfo.js';
 
+
+class StockApp{
+    constructor (dataURL) {
+        this.data = null;
+        this.fetchData(dataURL);
+    }
+
+    fetchData(sourceURL){
+        return fetch(sourceURL)
+            .then(response => response.json())
+            .then(data => this.onDataLoaded(data))
+    }
+
+    onDataLoaded(data){
+        this.data = data;
+        this.init();
+    }
+
+    init(){
+        console.log(this.data);
+    }
+}
+
+window.stockapp = new StockApp('../data/products.json');
